f0acb0af77187d1a12869e08cae53708ae087695
brokochan <brokochan@gmail.com>
2022-10-06 13:09:31 -0700

    tambah index php dan koneksikan ke database

--- dash/koneksi.php
+++ dash/koneksi.php
@@ -1,5 +1,5 @@
 <?php 
-$koneksi = new mysqli('host', 'user', 'password', 'database');
+$koneksi = new mysqli('localhost', 'dpmptgar_peron', 'Dodolgarut123_', 'dpmptgar_dtinvestasi');
 if ($koneksi->connect_errno) {
     die("gagal konek nih bos ".$koneksi->connect_error);
 }
